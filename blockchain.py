9cc8e8ac6fc06f7e406882ba525808146cb9f320
Aaron Pietsch <apietsch4@gmail.com>
2017-10-30 08:10:38 -1000

    add init, new trans methods

--- blockchain.py
+++ blockchain.py
@@ -1,15 +1,38 @@
+import json
+import hashlib
+from time import time
+
 class Blockchain(object):
 	def __init__(self):
 		self.chain = []
 		self.current_transactions = []
 
-	def new_block(self):
+		#create genesis block
+		self.new_block(previous_hash = 1, proof = 100)
+
+	def new_block(self, proof, previous_hash=None):
 		#creates a new block and adds to the blockchain
 		pass
 
-	def new_transaction(self):
+	def new_transaction(self, sender, recipient, amount):
 		#creates a new transaction and add it to the blockchain
-		pass 
+
+		"""
+
+		:param sender: address of the sender <str>
+		:param recipient: address of the recipient <str>
+		:param amount: amount <int>
+		:return: index of the block that will contain this trans
+
+		"""
+
+		self.current_transactions.append({
+			'sender': sender,
+			'recipient': recipient,
+			'amount': amount,
+			})
+
+		return self.last_block['index'] + 1
 
 	@staticmethod
 	def hash(block):
@@ -21,4 +44,3 @@ class Blockchain(object):
 		#returns the last block in the chain
 		pass
 
-	
