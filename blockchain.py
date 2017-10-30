a57a86d8b0bdde3a89516d713fd0f25ba242983f
Aaron Pietsch <apietsch4@gmail.com>
2017-10-30 08:22:30 -1000

    implement hashing and new trans

--- blockchain.py
+++ blockchain.py
@@ -10,9 +10,30 @@ class Blockchain(object):
 		#create genesis block
 		self.new_block(previous_hash = 1, proof = 100)
 
-	def new_block(self, proof, previous_hash=None):
+	def new_block(self, proof, previous_hash = None):
 		#creates a new block and adds to the blockchain
-		pass
+		
+		"""
+			
+		:param proof: the proof given by the proof of work algorithm <int>
+		:param previous_hash: (optional) the hash of prev block <int>
+		:return: new block <dict>
+
+		"""
+
+		block = {
+			'index' : len(self.chain) + 1,
+			'timestamp' : time(),
+			'transactions' : self.current_transactions,
+			'proof' : proof,
+			'previous_hash' : previous_hash or self.hash(self.chain[-1]),
+		}
+
+		#reset current transactions
+		self.current_transactions = [];
+
+		self.chain.append(block)
+		return block
 
 	def new_transaction(self, sender, recipient, amount):
 		#creates a new transaction and add it to the blockchain
@@ -34,13 +55,23 @@ class Blockchain(object):
 
 		return self.last_block['index'] + 1
 
-	@staticmethod
-	def hash(block):
-		#hashes a block
-		pass
 
 	@property
-	def last(block):
-		#returns the last block in the chain
-		pass
+	def last_block(self):
+		return self.chain[-1]
 
+	@staticmethod
+	def hash(block):
+		#hashes a block (SHA - 256)
+
+		"""
+
+		:param: block (dict)
+		:return: (str)
+
+		"""
+
+	#order dictionary
+	block_string = json.dumps(block, sort_keys = True).encode()
+
+	return hashlib.sha256(block_string).hexdigest()
